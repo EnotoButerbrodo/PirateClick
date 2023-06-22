9284771a27f81c8002a1c7451155ec02c397b0af
Igor Vasilev <Protanker25@gmail.com>
2023-06-22 17:15:38 +1000

    Рефакторинг Rotation

--- Assets/Code/Rotation/Rotation.cs
+++ Assets/Code/Rotation/Rotation.cs
@@ -5,7 +5,7 @@ namespace Code.Levels.Clicker
     public class Rotation : MonoBehaviour
     {
         [SerializeField][Range(0, 500f)] private float _rotationSpeed = 5f;
-        
+        [SerializeField] private Vector3 _rotationAxis = Vector3.up;
         private void Update()
         {
             RotateObject();
@@ -15,7 +15,7 @@ namespace Code.Levels.Clicker
         {
             var rotationSpeed = _rotationSpeed * Time.deltaTime;
             
-            transform.rotation *= Quaternion.AngleAxis(rotationSpeed, Vector3.up);
+            transform.rotation *= Quaternion.AngleAxis(rotationSpeed, _rotationAxis);
         }
     }
 }
