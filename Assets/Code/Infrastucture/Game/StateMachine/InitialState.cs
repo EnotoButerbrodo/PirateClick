52dd0236e6e2aaa9f608d69e7e144af470ab0d86
Igor Vasilev <Protanker25@gmail.com>
2023-07-09 17:10:44 +1000

    GameStateMachine

---
+++ Assets/Code/Infrastucture/Game/StateMachine/InitialState.cs
@@ -0,0 +1,24 @@
+﻿using EnotoButerbrodo.StateMachine;
+
+namespace Infrastructure
+{
+    public class InitialState : IState
+    {
+        private readonly GameStateMachine _context;
+
+        public InitialState(GameStateMachine context)
+        {
+            _context = context;
+        }
+
+        public void Enter()
+        {
+            
+        }
+
+        public void Exit()
+        {
+            
+        }
+    }
+}
